b212921e7033c66d11090dc189ef8849331ce01d
Bogdan Livadaru <bogdanlivadaru@yahoo.com>
2019-10-22 15:09:54 -0700

    Done

---
+++ Demo1022/Prob5/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob5
+{
+    class MyClass
+    {
+        public string McStr
+        {
+            get { return McStr }
+            set { McStr = value}
+        }
+
+        // if mc!=null { if string,compare;else if ints<McInt;else throw exception}
+
+
+
+        // return $"{McStr}, {McInt}"
+    }
+}
---
+++ Demo1022/Prob5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(mc1.CompareTo(mc2));
+
+            //sort array
+            //Array.Sort(mcArray);
+        }
+    }
+}
